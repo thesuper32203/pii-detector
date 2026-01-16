54e28206b4c3b116a5b201fcbeae157253c1199e
Anthony <thesuper32203@gmail.com>
2026-01-15 20:47:03 -0500

    configured and initialized model

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
+from transformers import pipeline
 
 app = Flask(__name__)
+
+# Function to load configuration from JSON file
+def load_config(config_path):
+    with open(config_path, "r") as f:
+        return json.load(f)
+
+# Function to initialize the hugging face pipeline
+def initialize_pipeline(config):
+    tasks = config.get("task","personal-information")
+    model = config.get("model")
+    tokenizer = config.get("tokenizer", None)
+    if tokenizer:
+        return pipeline(tasks,model=model,tokenizer=tokenizer)
+    return pipeline(tasks,model=model)
+
+config = load_config("config.json")
+pii_pipeline = initialize_pipeline(config)
+
 
 
 @app.route('/')
