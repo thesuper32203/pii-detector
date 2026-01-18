95a9a3ba5197770f80f426ba83051bc63bbec342
Anthony <thesuper32203@gmail.com>
2026-01-17 23:06:09 -0500

    cleaned up classes

--- app.py
+++ app.py
@@ -4,24 +4,9 @@ from transformers import pipeline
 
 app = Flask(__name__)
 
-# Function to load configuration from JSON file
-def load_config(config_path):
-    with open(config_path, "r") as f:
-        return json.load(f)
-
-# Function to initialize the hugging face pipeline
-def initialize_pipeline(config_pii):
-    tasks = config_pii.get("task", "token-classification")
-    model = config_pii.get("model")
-    aggregation_strategy = config_pii.get("aggregation_strategy")
-    tokenizer = config_pii.get("tokenizer", None)
-    if tokenizer:
-        return pipeline(tasks,model=model,tokenizer=tokenizer,aggregation_strategy=aggregation_strategy)
-    return pipeline(tasks,model=model,aggregation_strategy=aggregation_strategy)
 
 # Load the config file and initialize the pipeline
-config = load_config("config.json")
-pii = initialize_pipeline(config)
+
 text = "My name is John Doe. Email: john.doe@gmail.com. Phone: (212) 555-0199."
 results = pii(text)
 print(f"Processing: {text}")
---
+++ facehugging/config.py
@@ -0,0 +1,24 @@
+from transformers import pipeline
+from functools import lru_cache
+import json
+
+
+# Function to load configuration from JSON file
+def load_config(config_path):
+    with open(config_path, "r") as f:
+        return json.load(f)
+
+# Function to initialize the hugging face pipeline
+def initialize_pipeline(config_pii):
+    tasks = config_pii.get("task", "token-classification")
+    model = config_pii.get("model")
+    aggregation_strategy = config_pii.get("aggregation_strategy")
+    tokenizer = config_pii.get("tokenizer", None)
+    if tokenizer:
+        return pipeline(tasks,model=model,tokenizer=tokenizer,aggregation_strategy=aggregation_strategy)
+    return pipeline(tasks,model=model,aggregation_strategy=aggregation_strategy)
+
+@lru_cache(maxsize=1)
+def load_model():
+    config = load_config("facehugging/config.json")
+    return initialize_pipeline(config)
