01da7fc0a8b81352549adb3b90971396633a28c3
Anthony <thesuper32203@gmail.com>
2026-01-17 20:54:59 -0500

    saving progress

--- app.py
+++ app.py
@@ -10,16 +10,40 @@ def load_config(config_path):
         return json.load(f)
 
 # Function to initialize the hugging face pipeline
-def initialize_pipeline(config):
-    tasks = config.get("task","personal-information")
-    model = config.get("model")
-    tokenizer = config.get("tokenizer", None)
+def initialize_pipeline(config_pii):
+    tasks = config_pii.get("task", "token-classification")
+    model = config_pii.get("model")
+    aggregation_strategy = config_pii.get("aggregation_strategy")
+    tokenizer = config_pii.get("tokenizer", None)
     if tokenizer:
-        return pipeline(tasks,model=model,tokenizer=tokenizer)
-    return pipeline(tasks,model=model)
+        return pipeline(tasks,model=model,tokenizer=tokenizer,aggregation_strategy=aggregation_strategy)
+    return pipeline(tasks,model=model,aggregation_strategy=aggregation_strategy)
 
+# Load the config file and initialize the pipeline
 config = load_config("config.json")
-pii_pipeline = initialize_pipeline(config)
+pii = initialize_pipeline(config)
+text = "My name is John Doe. Email: john.doe@gmail.com. Phone: (212) 555-0199."
+results = pii(text)
+for r in results:
+    if r["score"] > 0.65:
+        #print(f"Token: {r}")
+        start = int(json.dumps(r["start"]))
+        end = int(json.dumps(r["end"]))
+        entity_group = json.dumps(r["entity_group"])
+        print(text)
+        text = text[:start] + entity_group + text[end:]
+        print(text[start:end])
+        #print(f"Before tokenization\n {text}")
+        #text[start:end] = entity_group
+        #print(f"After tokenization\n {text}")
+@app.route("/detect", methods=["POST"])
+def detect_pii():
+    data = request.json
+    text = data.get("text")
+    if not text:
+        return jsonify({"Error": "No text provided"})
+    pii_result = pii(text)
+
 
 
 
